4cae9c3f2a513c567edccad32b7e31e14db40f5e
Chris Rorden <rorden@mailbox.sc.edu>
2018-02-09 10:15:46 -0500

    Add Surf Ice mz3 format support (much smaller files than OBJ)

---
+++ mz3.pas
@@ -0,0 +1,186 @@
+unit mz3;
+{$IFDEF FPC}{$mode objfpc}{$H+}{$ENDIF}
+interface
+//load and save meshes in Surfice MZ3 format
+
+uses
+    zstream, Classes, sysutils,  meshify_simplify_quadric;
+
+procedure LoadMz3(const FileName: string; var faces: TFaces; var vertices: TVertices);
+procedure SaveMz3(const FileName: string; var faces: TFaces; var vertices: TVertices);
+
+implementation
+
+type
+   TRGBA = packed record //Next: analyze Format Header structure
+    R,G,B,A : byte;
+  end;
+  TFloats = array of single;
+  TVertexRGBA = array of TRGBA;
+
+function FSize (lFName: String): longint;
+var F : File Of byte;
+begin
+  result := 0;
+  if not fileexists(lFName) then exit;
+  Assign (F, lFName);
+  Reset (F);
+  result := FileSize(F);
+  Close (F);
+end;
+
+function LoadMz3Core(const FileName: string; var Faces: TFaces; var Vertices: TVertices; var vertexRGBA: TVertexRGBA; var intensity: TFloats): boolean;
+const
+ kMagic =  23117; //"MZ"
+ kChunkSize = 16384;
+label 666;
+var
+  i: integer;
+  bytes : array of byte;
+  Magic, Attr: uint16;
+  nFace, nVert, nSkip: uint32;
+  isFace, isVert, isRGBA, isScalar: boolean;
+  mStream : TMemoryStream;
+  zStream: TGZFileStream;
+begin
+     result := false;
+     setlength(Faces,0);
+     setlength(Vertices,0);
+     setlength(vertexRGBA,0);
+     setlength(intensity,0);
+     if not fileexists(Filename) then exit;
+     mStream := TMemoryStream.Create;
+     zStream := TGZFileStream.create(FileName, gzopenread);
+     setlength(bytes, kChunkSize);
+     repeat
+            i := zStream.read(bytes[0],kChunkSize);
+            mStream.Write(bytes[0],i) ;
+     until i < kChunkSize;
+     zStream.Free;
+     if mStream.Size < 72 then exit; //6 item header, 3 vertices (3 values, XYZ), 1 face (3 indices) each 4 bytes
+     mStream.Position := 0;
+     mStream.Read(Magic,2);
+     mStream.Read(Attr,2);
+     mStream.Read(nFace,4);
+     mStream.Read(nVert,4);
+     mStream.Read(nSkip,4);
+     if (magic <> kMagic) then goto 666;
+     isFace := (Attr and 1) > 0;
+     isVert := (Attr and 2) > 0;
+     isRGBA := (Attr and 4) > 0;
+     isScalar := (Attr and 8) > 0;
+     if (Attr > 15) then begin
+        writeln('Unsupported future format '+ inttostr(Attr));
+        goto 666;
+     end;
+     if (nFace = 0) and (isFace) then goto 666;
+     if (nVert = 0) and ((isVert) or (isRGBA) or (isScalar) ) then goto 666;
+     if nSkip > 0 then
+        mStream.Seek(nSkip, soFromCurrent);
+     result := true;
+     if isFace then begin
+        setlength(Faces,  nFace);
+        mStream.Read(Faces[0], nFace * 3 * sizeof(int32));
+     end;
+     if isVert then begin
+        setlength(Vertices,  nVert);
+        mStream.Read(Vertices[0], nVert * 3 * sizeof(single));
+     end;
+     if isRGBA then begin
+        setlength(vertexRGBA, nVert);
+        mStream.Read(vertexRGBA[0], nVert * 4 * sizeof(byte));
+     end;
+     if isScalar then begin
+        setlength(intensity, nVert);
+        mStream.Read(intensity[0], nVert * sizeof(single));
+     end;
+     result := true;
+   666 :
+     mStream.Free;
+end; // LoadMz3Core()
+
+procedure LoadMz3(const FileName: string; var faces: TFaces; var vertices: TVertices);
+var
+	vertexRGBA: TVertexRGBA;
+	intensity: TFloats;
+begin
+	LoadMz3Core(FileName, Faces, Vertices, vertexRGBA, intensity);
+	if length(vertexRGBA) > 0 then begin
+		writeln('Warning: ignoring vertex colors');
+		setlength(vertexRGBA, 0);
+	end;
+	if length(intensity) > 0 then begin
+		writeln('Warning: ignoring vertex intensities');
+		setlength(intensity, 0);
+	end;
+
+end;
+
+procedure SaveMz3Core(const FileName: string; Faces: TFaces; Vertices: TVertices; vertexRGBA: TVertexRGBA; intensity: TFloats);
+const
+ kMagic =  23117; //"MZ"
+var
+  isFace, isVert, isRGBA, isScalar: boolean;
+  Magic, Attr: uint16;
+  nFace, nVert, nSkip: uint32;
+  mStream : TMemoryStream;
+  zStream: TGZFileStream;
+  FileNameMz3: string;
+begin
+  nFace := length(Faces);
+  nVert := length(Vertices);
+  FileNameMz3 := changeFileExt(FileName, '.mz3');
+  isFace := nFace > 0;
+  isVert := nVert > 0;
+  isRGBA := false;
+  if length(vertexRGBA) > 0 then begin
+     isRGBA := true;
+     nVert := length(vertexRGBA);
+  end;
+  isScalar := false;
+  if length(intensity) > 0 then begin
+     isScalar := true;
+     nVert := length(intensity);
+  end;
+  if (nFace = 0) and (nVert = 0) then exit;
+  magic := kMagic;
+  Attr := 0;
+  if isFace then Attr := Attr + 1;
+  if isVert then Attr := Attr + 2;
+  if isRGBA then Attr := Attr + 4;
+  if isScalar then Attr := Attr + 8;
+  nSkip := 0; //do not pad header with any extra data
+  mStream := TMemoryStream.Create;
+  mStream.Write(Magic,2);
+  mStream.Write(Attr,2);
+  mStream.Write(nFace,4);
+  mStream.Write(nVert,4);
+  mStream.Write(nSkip,4);
+  if isFace then
+     mStream.Write(Faces[0], nFace * sizeof(TPoint3i));
+  if isVert  then
+     mStream.Write(Vertices[0], nVert * 3 * sizeof(single));
+  if isRGBA then
+     mStream.Write(vertexRGBA[0], nVert * 4 * sizeof(byte));
+  if isScalar then
+     mStream.Write(intensity[0], nVert * sizeof(single));
+  mStream.Position := 0;
+  zStream := TGZFileStream.Create(FileNameMz3, gzopenwrite);
+  zStream.CopyFrom(mStream, mStream.Size);
+  zStream.Free;
+  mStream.Free;
+end;
+
+procedure SaveMz3(const FileName: string; var faces: TFaces; var vertices: TVertices);
+var
+	vertexRGBA: TVertexRGBA;
+	intensity: TFloats;
+begin
+	setlength(vertexRGBA, 0);
+	setlength(intensity,0);
+	SaveMz3Core(FileName, faces, vertices, vertexRGBA, intensity);
+end;
+
+
+end.
+
--- simplify.pas
+++ simplify.pas
@@ -12,28 +12,52 @@ program simplify;
 
 {$IFDEF FPC}{$mode objfpc}{$H+}{$ENDIF}
 uses
- {$IFDEF FPC}{$IFNDEF DARWIN}DateUtils, {$ENDIF}{$ELSE} Windows, {$ENDIF}
+ {$IFDEF FPC} mz3,  {$IFNDEF DARWIN}DateUtils, {$ENDIF}{$ELSE} Windows, {$ENDIF}
  Classes, meshify_simplify_quadric, obj, sysutils;
 
 procedure ShowHelp;
 begin
+  {$IFDEF FPC}
 	writeln('Usage: '+paramstr(0)+' <input> <output> <ratio> <agressiveness)');
-	writeln(' Input: name of existing OBJ format mesh');
- 	writeln(' Output: name for decimated OBJ format mesh');
- 	writeln(' Ratio: (default = 0.5) for example 0.2 will decimate 80% of triangles');
- 	writeln(' Agressiveness: (default = 7.0) faster or better decimation');
-	writeln('Example :');
+	writeln(' Input: name of existing MZ3 or OBJ format mesh');
+ 	writeln(' Output: name for decimated MZ3 or OBJ format mesh');
+ 	writeln(' Ratio: (default = 0.2) for example 0.1 will decimate 90% of triangles');
+ 	writeln(' Agressiveness: (default = 2.0) faster (9) or better decimation (1)');
+ 	writeln('Notes:');
+ 	writeln(' The OBJ format is popular and useful for sharing files');
+ 	writeln(' The MZ3 format is creates more compact files');
+	writeln('Examples:');
 	{$IFDEF UNIX}
-	writeln(' '+paramstr(0)+' ~/dir/in.obj ~/dir/out.obj 0.2');
+	writeln(' '+paramstr(0)+' ~/dir/in.mz3 ~/dir/out.mz3 0.2');
+	writeln(' '+paramstr(0)+' ~/dir/bunny.obj ~/dir/out.obj 0.2');
+	writeln(' '+paramstr(0)+' ~/dir/bunny.obj ~/dir/out.mz3 0.2');
 	{$ELSE}
-	writeln(' '+paramstr(0)+' c:\dir\in.obj c:\dir\out.obj 0.2');
+	writeln(' '+paramstr(0)+' c:\dir\in.mz3 c:\dir\out.mz3 0.2');
+	writeln(' '+paramstr(0)+' c:\dir\bunny.obj c:\dir\out.obj 0.2');
+	writeln(' '+paramstr(0)+' c:\dir\bunny.obj c:\dir\out.mz3 0.2');
 	{$ENDIF}
+  {$ELSE} //if FPC else Delphi - Delphi does not support ZStreams
+    writeln('Usage: '+paramstr(0)+' <input> <output> <ratio> <agressiveness)');
+    writeln(' Input: name of existing OBJ format mesh');
+    writeln(' Output: name for decimated OBJ format mesh');
+    writeln(' Ratio: (default = 0.2) for example 0.1 will decimate 90% of triangles');
+    writeln(' Agressiveness: (default = 2.0) faster (9) or better decimation (1)');
+    writeln('Example:');
+    writeln(' '+paramstr(0)+' c:\dir\bunny.obj c:\dir\out.obj 0.2');
+  {$ENDIF}
 end;
 
 procedure printf(s: string); //for GUI applications, this would call showmessage or memo1.lines.add
 begin
      writeln(s);
 end;
+
+{$IFDEF FPC}
+function isMz3(filename: string): boolean;
+begin
+	result := upcase(ExtractFileExt(filename)) = '.MZ3';
+end;
+{$ENDIF}
 
 procedure DecimateMesh(inname, outname: string; ratio, agress: single);
 var
@@ -42,7 +66,13 @@ var
   vertices: TVertices;
     {$IFDEF FPC} {$IFDEF DARWIN} msec: qWord;{$ELSE} msec: Int64; tic :TDateTime; {$ENDIF} {$ELSE} msec: dWord; {$ENDIF}
 begin
-  LoadObj(inname, faces, vertices);
+  {$IFDEF FPC}
+  if isMz3(inname) then
+  	LoadMz3(inname, faces, vertices)
+  else
+  {$ENDIF}
+    LoadObj(inname, faces, vertices);
+  printf(format(' simplifying %s with a ratio of %.2f and agressiveness of %.2f', [inname, ratio, agress ]));
   startTri := length(faces);
   targetTri := round(length(faces) * ratio);
   if (targetTri < 0) or (length(faces) < 1) or (length(vertices) < 3) then begin
@@ -53,8 +83,15 @@ begin
   simplify_mesh(faces, vertices, targetTri, agress);
   {$IFDEF FPC} {$IFDEF DARWIN} msec := GetTickCount64()-msec; {$ELSE}msec := MilliSecondsBetween(Now(),tic);{$ENDIF} {$ELSE} msec := GetTickCount() - msec; {$ENDIF}
   printf(format(' number of triangles reduced from %d to %d (%.3f, %.2fsec)', [startTri, length(Faces), length(Faces)/startTri, msec*0.001  ]));
-  if length(outname) > 0 then
-     SaveObj(outname, faces, vertices);
+  if length(outname) > 0 then begin
+  	printf('  Creating file '+ outname);
+  	{$IFDEF FPC}
+        if isMz3(outname) then
+  	  SaveMz3(outname, faces, vertices)
+  	else
+        {$ENDIF}
+  	  SaveObj(outname, faces, vertices);
+  end;
   setlength(faces,0);
   setlength(vertices,0);
 end;
@@ -65,22 +102,29 @@ var
 	ratio, agress: single;
 begin
 	printf('Mesh Simplification (C)2014 by Sven Forstmann, MIT License '+{$IFDEF CPU64}'64-bit'{$ELSE}'32-bit'{$ENDIF});
-	if ParamCount < 2 then begin
+	if ParamCount < 1 then begin
   		ShowHelp;
   		exit;
   	end;
   	inname := paramstr(1);
-  	outname := paramstr(2);
-  	ratio := 0.5;
+  	if ParamCount < 2 then begin
+                {$IFDEF FPC}
+                outname := ChangeFileExt(inname, '_simple.mz3');
+                {$ELSE}
+                outname := ChangeFileExt(inname, '_simple.obj');
+                {$ENDIF}
+  	end else
+  		outname := paramstr(2);
+  	ratio := 0.2;
   	if ParamCount > 2 then
   		ratio := StrToFloatDef(paramstr(3),0.5);
   	if (ratio <= 0.0) or (ratio >= 1.0) then begin
   		printf('Ratio must be more than zero and less than one.');
   		exit;
   	end;
-  	agress := 7.0;
+  	agress := 2.0;
   	if ParamCount > 2 then
-  		agress := StrToFloatDef(paramstr(4),7.0);
+  		agress := StrToFloatDef(paramstr(4),2.0);
 	DecimateMesh(inname, outname, ratio, agress);
 end;
 
